11553455afa6b49c17c65f3512feebc28bee4b78
kellian <germainkellian@gmail.com>
2021-12-21 15:28:42 +0100

    ennemis prefab + desactivation light pour facilité les tests

---
+++ Assets/Script/ennemis/Ennemis.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemis : MonoBehaviour
+{
+    public int[] pointA; //[x,y]
+    public int[] pointB; //[x,y]
+
+    public int vitesse;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
