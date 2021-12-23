e3c3d957e900d96a25c236d88d006d6c88f2de1b
flguerin <florentguerin41@gmail.com>
2021-12-23 15:51:20 +0100

    Modification script déplacement pour vitesse

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
     //Attributs
-    public float movingSpeed;
+    public float movingSpeed= 0.3f;
     public float smoothTime;
     public bool BombSet=false;
+   
 
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
 
     public Bomb bomb;
+    private float startTime;
+    private float holdTime;
 
     void Start(){
-        
+        startTime = Time.time;
+        holdTime = 0.0f;
+        movingSpeed = 0.5f;
     }
 
     // Update is called once per frame
@@ -25,28 +31,112 @@ public class PlayerMovement : MonoBehaviour
         float verticalMovement = Input.GetAxis("Vertical") * movingSpeed * Time.deltaTime;
         MovePlayer(horizontalMovement, verticalMovement); */
         MapItem[,] mapItemsList = GameObject.Find("Map").GetComponent<Map>().mapItemsList;
+        
+        
+        bool firstUp = false;
+        bool firstDown = false;
+        bool firstLeft = false;
+        bool firstRight = false;
+
 
 
         int x = (int)this.transform.position.x;
         int y = (int)this.transform.position.y;
-        if (Input.GetKeyDown(KeyCode.UpArrow) && mapItemsList[x,y+1] is Sol) {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
             Debug.Log("Up");
-        	MovePlayer(x,y+1);
+            if (movingSpeed <= (float)Time.time - (float)startTime)
+            {
+                startTime = (float)Time.time;
+                firstUp = true;
+            }
+            
         }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow) && mapItemsList[x,y-1] is Sol) {
+        if ((Input.GetKey(KeyCode.UpArrow) || firstUp) && mapItemsList[x,y+1] is Sol) {
+            
+            holdTime = (float)Time.time - (float)startTime;
+            Debug.Log(holdTime);
+            if ( movingSpeed <= holdTime || firstUp)
+            {
+                MovePlayer(x, y + 1);
+                startTime = Time.time;
+                firstUp = false;
+            }
+        	
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
             Debug.Log("Down");
-        	MovePlayer(x,y-1);
+            if (movingSpeed <= (float)Time.time - (float)startTime)
+            {
+                startTime = (float)Time.time;
+                firstDown = true;
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && mapItemsList[x-1,y] is Sol) {
+        if ((Input.GetKey(KeyCode.DownArrow) || firstDown) && mapItemsList[x, y - 1] is Sol)
+        {
+
+            holdTime = (float)Time.time - (float)startTime;
+            Debug.Log(holdTime);
+            if (movingSpeed <= holdTime || firstDown)
+            {
+                MovePlayer(x, y - 1);
+                startTime = Time.time;
+                firstDown = false;
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
             Debug.Log("Left");
-        	MovePlayer(x-1,y);
+            if (movingSpeed <= (float)Time.time - (float)startTime)
+            {
+                startTime = (float)Time.time;
+                firstLeft = true;
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow) && mapItemsList[x+1,y] is Sol) {
+        if ((Input.GetKey(KeyCode.LeftArrow) || firstLeft) && mapItemsList[x-1,y] is Sol)
+        {
+
+            holdTime = (float)Time.time - (float)startTime;
+            Debug.Log(holdTime);
+            if (movingSpeed <= holdTime || firstLeft)
+            {
+                MovePlayer(x-1, y);
+                startTime = Time.time;
+                firstLeft = false;
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
             Debug.Log("Right");
-        	MovePlayer(x+1,y);
+            if (movingSpeed <= (float)Time.time - (float)startTime)
+            {
+                startTime = (float)Time.time;
+                firstRight = true;
+            }
+        }
+
+        if ((Input.GetKey(KeyCode.RightArrow) || firstRight) && mapItemsList[x + 1, y] is Sol)
+        {
+
+            holdTime = (float)Time.time - (float)startTime;
+            Debug.Log(holdTime);
+            if (movingSpeed <= holdTime || firstRight)
+            {
+                MovePlayer(x + 1, y);
+                startTime = Time.time;
+                firstRight = false;
+            }
+
         }
 
         if (Input.GetKeyDown(KeyCode.B) && !BombSet)
