3e2f21430057e7d3b1ab228d26a3f178d29b3c85
kellian <germainkellian@gmail.com>
2021-12-29 18:13:23 +0100

    script zombie + bug player -> retrait du rigidbody de joueur

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Attributs
-    public float movingSpeed;
-    public float smoothTime;
     public bool BombSet=false;
-
-    public Rigidbody2D rb;
-    private Vector3 velocity = Vector3.zero;
 
     public Bomb bomb;
 
@@ -21,26 +15,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /* float horizontalMovement = Input.GetAxis("Horizontal") * movingSpeed * Time.deltaTime;
-        float verticalMovement = Input.GetAxis("Vertical") * movingSpeed * Time.deltaTime;
-        MovePlayer(horizontalMovement, verticalMovement); */
         MapItem[,] mapItemsList = GameObject.Find("Map").GetComponent<Map>().mapItemsList;
         int x = (int)this.transform.position.x;
         int y = (int)this.transform.position.y;
         if (Input.GetKeyDown(KeyCode.UpArrow) && mapItemsList[x,y+1] is Sol) {
+            Debug.Log("deplacement joueur haut");
         	MovePlayer(x,y+1);
         }
 
         if (Input.GetKeyDown(KeyCode.DownArrow) && mapItemsList[x,y-1] is Sol) {
-        	MovePlayer(x,y-1);
+            Debug.Log("deplacement joueur bas");
+            MovePlayer(x,y-1);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow) && mapItemsList[x-1,y] is Sol) {
-        	MovePlayer(x-1,y);
+            Debug.Log("deplacement joueur gauche");
+            MovePlayer(x-1,y);
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow) && mapItemsList[x+1,y] is Sol) {
-        	MovePlayer(x+1,y);
+            Debug.Log("deplacement joueur droit");
+            MovePlayer(x+1,y);
         }
 
         if (Input.GetKeyDown(KeyCode.B) && !BombSet)
@@ -53,8 +48,7 @@ public class PlayerMovement : MonoBehaviour
 
     void MovePlayer(float _horizontalMovement, float _verticalMovement)
     {
-        /*Vector3 targetVelocity = new Vector2(_horizontalMovement, _verticalMovement);
-        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, smoothTime);*/
         transform.position = new Vector3(_horizontalMovement, _verticalMovement, 0);
+        Debug.Log("position actuel :" + _horizontalMovement + ", " + _verticalMovement);
     }
 }
--- Assets/Script/ennemis/Ennemis.cs
+++ Assets/Script/ennemis/Ennemis.cs
@@ -1,29 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Ennemis : MonoBehaviour
 {
     public int scoreValue;
-    public int[] pointA; //[x,y]
-    public int[] pointB; //[x,y]
+
+    public Transform[] waypoints;
+    public Transform target;
+    public int dest = 0;
+
+    public Path path;
+    public int currentWaypoint = 0;
+    public bool reachedEndOfPath = false;
+
+    private Seeker seeker;
 
     public int vitesse;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        seeker = GetComponent<Seeker>();
+        target = waypoints[0];
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+             seeker.StartPath(this.transform.position, target.position, OnPathComplete);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(path == null)
+        {
+            return;
+        }
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        float distance = Vector2.Distance(this.transform.position, path.vectorPath[currentWaypoint]);
+
+        if(distance < 1f)
+        {
+            currentWaypoint++;
+        }
         
+        if(Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            dest = (dest + 1) % waypoints.Length;
+            target = waypoints[dest];
+        }
     }
 
     public void Kill() { 
         GameObject.Find("GameMaster").GetComponent<ScoreManager>().scoreNiveau += scoreValue;
         Destroy(gameObject);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
     }
 }
