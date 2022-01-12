4ce47b912c331d935b895cb6eae7801810ac1ad7
Desrayaud <desrayaudcharles@gmail.com>
2022-01-12 15:55:32 +0100

    Menu Paramètre inactif

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -4,13 +4,14 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public string s;
+    public GameObject p;
     public void NouvellePartie(){
-       SceneManager.LoadScene(s);
+        SceneManager.LoadScene(s);
     }
     public void Quitter(){
         Application.Quit();
     }
     public void ParametreMenu(){
-        //parametre.SetActive(true);
+        p.SetActive(true);
     }
 }
