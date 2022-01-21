2581690b4f3be956446cb9c3b5bfc89deb011df0
mrochard <matthieu.rochard@etudiant.univ-rennes1.fr>
2022-01-21 14:21:40 +0100

    Correction du merge avec editeur

--- Assets/Script/Menu/MainMenu.cs
+++ Assets/Script/Menu/MainMenu.cs
@@ -1,28 +1,59 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Script du menu pricipale
+/// </summary>
 public class MainMenu : MonoBehaviour
 {
     public string s1;
     public string s2;
     public string s3;
     public GameObject p;
-    public void NouvellePartie(){
-        SceneManager.LoadScene(s1);
-    }
-    public void Quitter(){
-        Application.Quit();
-    }
-    public void ParametreMenu(){
-        p.SetActive(true);
+    public GameObject saves;
+
+    public void Continue()
+    {
+        gameObject.SetActive(false);
+        saves.SetActive(true);
     }
 
-    public void Editeur(){
-        SceneManager.LoadScene(s2);
+    public void Quitter()
+    {
+        PlayerPrefs.Save();
+        Application.Quit();
+    }
+
+    public void ParametreMenu()
+    {
+        gameObject.SetActive(false);
+        p.SetActive(true);
+    }
+    public void Editeur()
+    {
+        SceneManager.LoadScene("LevelCreator");
     }
 
     public void CreatedLevel()
     {
-        SceneManager.LoadScene(s3);
+        SceneManager.LoadScene("CreatedLevel");
+    }
+
+    void OnEnable()
+    {
+        //Tell our 'OnLevelFinishedLoading' function to start listening for a scene change as soon as this script is enabled.
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnDisable()
+    {
+        //Tell our 'OnLevelFinishedLoading' function to stop listening for a scene change as soon as this script is disabled. Remember to always have an unsubscription for every delegate you subscribe to!
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        //PlayerPrefs.DeleteAll();
+        saves.GetComponent<MenuSaves>().SaveGame();
     }
 }
--- Assets/Script/ennemis/CreatedLevel/EnnemisCreator.cs
+++ Assets/Script/ennemis/CreatedLevel/EnnemisCreator.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Pathfinding;
 
 public abstract class EnnemisCreator : MonoBehaviour
 {
